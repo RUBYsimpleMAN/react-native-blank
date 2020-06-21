d6d90da27af483924748b17765c418a5349c00b4
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-06-21 11:38:30 +0300

    2020-06-21_11-38-30_PicturesWorks!

--- App.js
+++ App.js
@@ -1,23 +1,13 @@
-import React, {useState} from 'react'
-import {StyleSheet, Text, View, ScrollView, Keyboard, Alert} from 'react-native'
-import {Navbar} from './src/components/Navbar'
-import {MainScreen} from './src/screens/MainScreen'
-import {TodoScreen} from './src/screens/TodoScreen'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, ScrollView, Keyboard, Alert } from 'react-native'
+import { Navbar } from './src/components/Navbar'
+import { MainScreen } from './src/screens/MainScreen'
+import { TodoScreen } from './src/screens/TodoScreen'
 
 export default function App() {
-  const [todoId, setTodoId] = useState('7') // null
+  const [todoId, setTodoId] = useState(null)
   const [todos, setTodos] = useState([
-    {id: '1',  title: 'Дело номер РАЗ' },
-    {id: '2',  title: 'Дело номер ДВА' },
-    {id: '3',  title: 'Дело номер ТРИ' },
-    {id: '4',  title: 'Дело номер ЧЕТЫРЕ' },
-    {id: '5',  title: 'Дело номер ПЯТЬ' },
-    {id: '6',  title: 'Дело номер ШЕСТЬ' },
-    {id: '7',  title: 'Дело номер СЕМЬ' },
-    {id: '8',  title: 'Дело номер ВОСЕМЬ' },
-    {id: '9',  title: 'Дело номер ДЕВЯТЬ' },
-    {id: '10', title: 'Дело номер ДЕСЯТЬ' },
-    {id: '11', title: 'Дело номер ОДИННАДЦАТЬ' }
+    // {id: '1',  title: 'Дело номер РАЗ' }
   ])
 
   const addTodo = title => {
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,28 +1,42 @@
 import React from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
-import {AddToDoItem} from '../components/AddToDoItem'
-import {Todo}        from '../components/TemplateToDo'
+import { View, Image, FlatList, StyleSheet } from 'react-native';
+import { AddToDoItem } from '../components/AddToDoItem'
+import { Todo }        from '../components/TemplateToDo'
+import { THEME } from '../themes/themes';
 
 export const MainScreen = ({addTodo, todos, rmTodoItem, openTodoItem}) => {
+
+  let mainScreenContent = (
+    <FlatList
+    keyExtractor={item => item.id.toString()}
+    data={todos}
+    renderItem={({item}) => (
+      <Todo todo={item}
+            onRemove={rmTodoItem}
+            onOpen={openTodoItem} />
+    )} />
+  )
+
+  if (todos.length === 0) {
+    mainScreenContent = (
+      <View style={styles.imgWrap}>
+        <Image  style={styles.img}
+                source={require('../../assets/splash.png')} />
+      </View>
+    )
+  }
+
   return(
     <View style={styles.mainScreenView}>
       <AddToDoItem onSubmit={addTodo}/>
-      <FlatList
-        keyExtractor={item => item.id.toString()}
-        data={todos}
-        renderItem={({item}) => (
-          <Todo todo={item}
-                onRemove={rmTodoItem}
-                onOpen={openTodoItem} />
-        )}
-      />
+      { mainScreenContent }
     </View>
   )
 };
 
 const styles = StyleSheet.create({
   mainScreenView: {
-    backgroundColor: 'blue',
+    backgroundColor: THEME.BRAND_COLOR,
   },
   mainScreenText: {
     textAlign: 'center',
@@ -30,5 +44,18 @@ const styles = StyleSheet.create({
     fontSize: 24,
     letterSpacing: 2,
     lineHeight: 48
+  },
+  imgWrap: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    // padding: 40,
+    height:500
+  },
+  img: {
+    width: '60%',
+    height: '60%',
+    resizeMode: 'contain',
+    // padding: 40,
+    // height:256
   }
 })
