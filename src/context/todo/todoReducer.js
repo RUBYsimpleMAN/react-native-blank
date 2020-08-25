1abdd8d38cadfa953bb1cd667fce46d238cc95d0
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-08-25 10:35:12 +0300

    2020-08-25_10-35-12_todoReducerReady

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,3 +1,19 @@
+import {  CREATE_TODO, UPDATE_TODO, DELETE_TODO } from './todoActionTypes'
+
+// const initialState = []
+
 export const todoReducer = (state, action) => {
-  return state
+  switch(action.type) {
+    case CREATE_TODO: return {...state, todos: [...state.todos, { id: Date.now().toString(),
+                                                                  title: action.title }
+    ]}
+    case UPDATE_TODO: return {...state, todos: state.todos.map(todo => {
+                                                              if (todo.id === action.id) {
+                                                                todo.title = action.title
+                                                              }
+                      return todo
+    })}
+    case DELETE_TODO: return {...state, todos: state.todos.filter(todo.id !== action.id)}
+    default:          return state
+  }
 }
