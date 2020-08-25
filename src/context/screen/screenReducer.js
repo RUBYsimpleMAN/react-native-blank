3f808d937475daf310b544af42d59f9a01de013b
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-08-25 16:18:38 +0300

    2020-08-25_16-18-38_coupleStreengsToReducer

--- src/context/screen/screenReducer.js
+++ src/context/screen/screenReducer.js
@@ -1,3 +1,11 @@
+import { TOGGLE_SCREEN } from "./screenActionTypes"
+
+const handlers = {
+  [TOGGLE_SCREEN]: (state, payload) => payload,
+  DEFAULT: state => state
+}
+
 export const screenReducer = (state, action) => {
-  return state
+  const handler = handlers[action.type] || handlers.DEFAULT
+  return handler(state, action.payload)
 }
