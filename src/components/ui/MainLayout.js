81dd4b775400d5a4e757ab97b9065908d505e299
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-08-26 19:47:17 +0300

    2020-08-26_19-47-17_MainLayoutForTodoScreenContextApplied

--- src/components/ui/MainLayout.js
+++ src/components/ui/MainLayout.js
@@ -1,19 +1,16 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { Alert, Keyboard, View, StyleSheet } from 'react-native'
 
 import { THEME }       from '../../themes/themes'
 import { Navbar }      from '../../components/Navbar'
 import { MainScreen }  from '../../screens/MainScreen'
 import { TodoScreen }  from '../../screens/TodoScreen'
-import { TodoContext } from '../../context/todo/todoContext'
 import { ScreenContext } from '../../context/screen/screenContext'
 
 
 //export default function MainLayout() {
 export const MainLayout = () => {
-  const { todos, addTodo, updateTodo, rmTodoItem } = useContext(TodoContext)
-  const { todoId, toggleScreen } = useContext(ScreenContext)
-
+  const { todoId } = useContext(ScreenContext)
 
   // const rmTodoItem = id => {
   //   const todo = todos.find(t => t.id === id)
@@ -39,19 +36,12 @@ export const MainLayout = () => {
   //   );
   // }
 
-
-  let content = <MainScreen />
-
-  if (todoId) {
-      content = <TodoScreen />
-  }
-
   return (
     <View  style={styles.appScoupe}
            onPress={() => Keyboard.dismiss()}>
       <Navbar title='SclerozzIO' />
       <View style={styles.container}>
-        {content}
+        {todoId ? <TodoScreen /> : <MainScreen />}
       </View>
     </View>
   );
